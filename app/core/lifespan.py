181fd34ba69ddcd4b166be67388024763affa2c7
Joao Marcos <joao.marcos.witcel@gmail.com>
2026-08-02 00:18:59 -0300

    novas dependências

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -1,11 +1,15 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from llama_index.core import Settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Application Startup")
     print("Recuperando recursos globais...")
+
+    # Desabilitando LLM
+    Settings.llm = None
 
     yield
 
